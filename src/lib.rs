55010489bab006461644076d0779aee865861245
Frank McSherry <fmcsherry@me.com>
2015-02-21 21:11:47 +0000

    introducing associated types

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,25 @@
+#![feature(test)]
+#![feature(old_io)]
+
 extern crate test;
 
 use std::mem;
 use std::mem::{transmute, size_of, replace};
 use std::default::Default;
 use std::string::String;
-use std::io::{IoResult, MemReader, MemWriter};
+use std::old_io::{IoResult, MemReader, MemWriter};
+use std::marker::PhantomFn;
 
 use test::Bencher;
 
-// this trait defines a "prefered implementation" of ColumnarVec for a type T.
-// because multiple types may implement, for example, ColumnarVec<(uint, uint)>.
-pub trait Columnar<R: ColumnarVec<Self>> { }
+// this trait defines a "prefered implementation" of ColumnarStack for a type T,
+// because multiple types may implement, for example, ColumnarStack<(u64, u64)>.
+pub trait Columnar: PhantomFn<Self> {
+    type Stack: ColumnarStack<Self>;
+}
 
-pub trait ColumnarVec<T> : Default {
+// this trait defines a push/pop interface backed by easily serialized columnar storage.
+pub trait ColumnarStack<T> : Default {
     fn push(&mut self, T);
     fn pop(&mut self) -> Option<T>;
 
@@ -20,14 +27,14 @@ pub trait ColumnarVec<T> : Default {
     fn decode(&mut self, &mut Vec<Vec<u8>>);
 
     // default implementation of writing to a Writer via encode
-    // note: writes out data but does *not* clear the ColumnarVec
+    // note: writes out data but does *not* clear the ColumnarStack
     fn write<W: Writer>(&mut self, writer: &mut W) -> IoResult<()> {
         let mut bytes = Vec::new();
         self.encode(&mut bytes);
 
         for vec in bytes.iter() {
             try!(writer.write_le_u64(vec.len() as u64));
-            try!(writer.write(vec.as_slice()));
+            try!(writer.write_all(&vec[..]));
         }
 
         // re-install bytes
@@ -53,52 +60,47 @@ pub trait ColumnarVec<T> : Default {
     }
 }
 
-// implementations defining default implementors of ColumnarVec.
-impl Columnar<Vec<i64>> for i64 { }
-impl Columnar<Vec<i32>> for i32 { }
-impl Columnar<Vec<i16>> for i16 { }
+// implementations defining default implementors of ColumnarStack.
+impl Columnar for i64 { type Stack = Vec<i64>; }
+impl Columnar for i32 { type Stack = Vec<i32>; }
+impl Columnar for i16 { type Stack = Vec<i16>; }
 
-impl Columnar<Vec<u64>> for u64 { }
-impl Columnar<Vec<u32>> for u32 { }
-impl Columnar<Vec<u16>> for u16 { }
-impl Columnar<Vec<u8>> for u8 { }
+impl Columnar for u64 { type Stack = Vec<u64>; }
+impl Columnar for u32 { type Stack = Vec<u32>; }
+impl Columnar for u16 { type Stack = Vec<u16>; }
+impl Columnar for u8  { type Stack = Vec<u8>;  }
 
-impl Columnar<Vec<usize>> for usize { }
-impl Columnar<Vec<isize>> for isize { }
+impl Columnar for usize { type Stack = Vec<usize>; }
+impl Columnar for isize { type Stack = Vec<isize>; }
 
-impl Columnar<(Vec<u64>, Vec<u8>, Vec<Vec<u8>>)> for String { }
+impl Columnar for String { type Stack = (Vec<u64>, Vec<u8>, Vec<Vec<u8>>); }
 
-impl<T1: Columnar<R1>, R1: ColumnarVec<T1>, T2: Columnar<R2>, R2: ColumnarVec<T2>> Columnar<(R1, R2)> for (T1, T2) { }
+impl<T1: Columnar, T2: Columnar> Columnar for (T1, T2) { type Stack = (T1::Stack, T2::Stack); }
 
-impl<T: Columnar<R>, R: ColumnarVec<T>> Columnar<(Vec<u8>, R)> for Option<T> { }
-impl<T: Columnar<R>, R: ColumnarVec<T>> Columnar<(Vec<u64>, R, Vec<Vec<T>>)> for Vec<T> { }
-
-
+impl<T: Columnar> Columnar for Option<T> { type Stack = (Vec<u8>, T::Stack); }
+impl<T: Columnar> Columnar for Vec<T> { type Stack = (Vec<u64>, T::Stack, Vec<Vec<T>>); }
 
 
-impl<T:Copy> ColumnarVec<T> for Vec<T> {
-    #[inline(always)]
-    fn push(&mut self, data: T) { self.push(data); }
-
-    #[inline(always)]
-    fn pop(&mut self) -> Option<T> { self.pop() }
+// implementations of specific ColumnarQueues.
+impl<T:Copy> ColumnarStack<T> for Vec<T> {
+    #[inline(always)] fn push(&mut self, data: T) { self.push(data); }
+    #[inline(always)] fn pop(&mut self) -> Option<T> { self.pop() }
 
     fn encode(&mut self, buffers: &mut Vec<Vec<u8>>) {
         buffers.push(unsafe { to_bytes_vec(replace(self, Vec::new())) });
     }
 
     fn decode(&mut self, buffers: &mut Vec<Vec<u8>>) {
-        if self.len() > 0 { panic!("calling decode from a non-empty ColumnarVec"); }
+        if self.len() > 0 { panic!("calling decode from a non-empty ColumnarStack"); }
         *self = unsafe { to_typed_vec(buffers.pop().unwrap()) };
     }
 }
 
 
-impl ColumnarVec<String> for (Vec<u64>, Vec<u8>, Vec<Vec<u8>>) {
+impl ColumnarStack<String> for (Vec<u64>, Vec<u8>, Vec<Vec<u8>>) {
     #[inline(always)]
     fn push(&mut self, string: String) {
         let mut vector = string.into_bytes();
-
         self.0.push(vector.len() as u64);
         while let Some(record) = vector.pop() { self.1.push(record); }
         self.2.push(vector);
@@ -106,10 +108,8 @@ impl ColumnarVec<String> for (Vec<u64>, Vec<u8>, Vec<Vec<u8>>) {
 
     #[inline(always)]
     fn pop(&mut self) -> Option<String> {
-
         if let Some(count) = self.0.pop() {
             let mut vector = self.2.pop().unwrap_or(Vec::new());
-            // if vector.capacity() == 0 { println!("empty string!"); }
             for _ in (0..count) { vector.push(self.1.pop().unwrap()); }
             Some(unsafe { String::from_utf8_unchecked(vector) })
         }
@@ -129,9 +129,7 @@ impl ColumnarVec<String> for (Vec<u64>, Vec<u8>, Vec<Vec<u8>>) {
 
 
 
-impl<T1, R1, T2, R2> ColumnarVec<(T1, T2)> for (R1, R2)
-where R1: ColumnarVec<T1>,
-      R2: ColumnarVec<T2>, {
+impl<T1: Columnar, T2: Columnar> ColumnarStack<(T1, T2)> for (T1::Stack, T2::Stack) {
     #[inline(always)]
     fn push(&mut self, (x, y): (T1, T2)) {
         self.0.push(x);
@@ -153,7 +151,7 @@ where R1: ColumnarVec<T1>,
     }
 }
 
-impl<T, R: ColumnarVec<T>> ColumnarVec<Option<T>> for (Vec<u8>, R) {
+impl<T, R: ColumnarStack<T>> ColumnarStack<Option<T>> for (Vec<u8>, R) {
     #[inline(always)]
     fn push(&mut self, option: Option<T>) {
         match option {
@@ -182,7 +180,7 @@ impl<T, R: ColumnarVec<T>> ColumnarVec<Option<T>> for (Vec<u8>, R) {
     }
 }
 
-impl<T, R1: ColumnarVec<u64>, R2: ColumnarVec<T>> ColumnarVec<Vec<T>> for (R1, R2, Vec<Vec<T>>) {
+impl<T, R1: ColumnarStack<u64>, R2: ColumnarStack<T>> ColumnarStack<Vec<T>> for (R1, R2, Vec<Vec<T>>) {
     #[inline(always)]
     fn push(&mut self, mut vector: Vec<T>) {
         self.0.push(vector.len() as u64);
@@ -263,21 +261,23 @@ fn mem_option_u64(bencher: &mut Bencher) {
 
 #[bench]
 fn u64_vec_string_u64(bencher: &mut Bencher) {
-    let data: Vec<(u64,Vec<_>)> = (0..128u64).map(|i| (i, (0..5u64).map(|j| (format!("number: {}", i + j), i as u64 + 10)).collect())).collect();
+    let data: Vec<(u64,Vec<_>)> = (0..128u64).map(|i| (i, (0..5u64).map(|j| (format!("number: {}", i + j), i as u64 + 10)).collect()))
+                                             .collect();
     _bench_enc_dec(bencher, data);
 }
 
 #[bench]
 fn mem_u64_vec_string_u64(bencher: &mut Bencher) {
-    let data: Vec<(u64,Vec<_>)> = (0..128u64).map(|i| (i, (0..5u64).map(|j| (format!("number: {}", i + j), i as u64 + 10)).collect())).collect();
+    let data: Vec<(u64,Vec<_>)> = (0..128u64).map(|i| (i, (0..5u64).map(|j| (format!("number: {}", i + j), i as u64 + 10)).collect()))
+                                             .collect();
     _bench_mem_wr(bencher, data);
 }
 
 
 // bounces some elements back and forth between columnar stacks, encoding/decoding ...
-fn _bench_enc_dec<T: Columnar<R>+Eq+PartialEq+Clone, R: ColumnarVec<T>>(bencher: &mut Bencher, mut elements: Vec<T>) {
-    let mut stack1: R = Default::default();
-    let mut stack2: R = Default::default();
+fn _bench_enc_dec<T: Columnar+Eq+PartialEq+Clone>(bencher: &mut Bencher, mut elements: Vec<T>) {
+    let mut stack1: T::Stack = Default::default();
+    let mut stack2: T::Stack = Default::default();
 
     let mut buffers = Vec::new();
 
@@ -313,10 +313,8 @@ fn _bench_enc_dec<T: Columnar<R>+Eq+PartialEq+Clone, R: ColumnarVec<T>>(bencher:
     }
 }
 
-fn _bench_mem_wr<T, R>(bencher: &mut Bencher, mut elements: Vec<T>)
-where T: Columnar<R>,
-      R: ColumnarVec<T>, {
-    let mut stack: R = Default::default();
+fn _bench_mem_wr<T: Columnar>(bencher: &mut Bencher, mut elements: Vec<T>) {
+    let mut stack: T::Stack = Default::default();
     let mut bytes = 0;
 
     while let Some(record) = elements.pop() { stack.push(record); }
