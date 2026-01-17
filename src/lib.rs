372b3a99f06dd0fa97442c88531a83da4ebe8e33
Frank McSherry <fmcsherry@me.com>
2026-01-17 14:15:03 -0500

    Re-add broken roaring, to avoid semver break

--- src/lib.rs
+++ src/lib.rs
@@ -635,6 +635,23 @@ pub mod common {
 
 }
 
+/// Roaring bitmap (and similar) containers.
+pub mod roaring {
+
+    use crate::Results;
+
+    /// A container for `bool` that uses techniques from Roaring bitmaps.
+    ///
+    /// These techniques are to block the bits into blocks of 2^16 bits,
+    /// and to encode each block based on its density. Either a bitmap
+    /// for dense blocks or a list of set bits for sparse blocks.
+    ///
+    /// Additionally, other representations encode runs of set bits.
+    pub struct RoaringBits {
+        _inner: Results<[u64; 1024], Vec<u16>>,
+    }
+}
+
 pub use chain_mod::{chain, chain_one};
 
 pub mod chain_mod {
